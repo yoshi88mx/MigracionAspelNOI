a7b839ff5d14b24c6df5cebf4c8f99c542507da4
Emilio Yoshimar Lopez Esponda <yoshi88mx@hotmail.com>
2022-03-22 17:52:16 -0600

    Ejecucion desde carpeta de dacaspel y creacion de carpetas de reportes para cada empresa

--- MigracionAspelNOI/Program.cs
+++ MigracionAspelNOI/Program.cs
@@ -8,42 +8,62 @@ namespace MigracionAspelNOI
     {
         static void Main(string[] args)
         {
-            if (args.Any())
+            var carpetaRaiz = Directory.GetCurrentDirectory();
+            var carpetaBasesDatos = Path.Combine(carpetaRaiz, "Datos");
+            if (Directory.Exists(carpetaBasesDatos))
             {
-                if (Directory.Exists(args[0]))
+                Console.WriteLine($"{nameof(carpetaBasesDatos)}:{carpetaBasesDatos}");
+                var carpetas = Directory.GetDirectories(carpetaBasesDatos);
+                var carpetaPlantillas = Path.Combine(carpetaRaiz, "Plantillas");
+                var contadorCarpetaReportes = 1;
+                foreach (var carpeta in carpetas)
                 {
-                    var carpetaRaiz = args[0];
-                    var carpetaBasesDatos = Path.Combine(args[0], "Datos");
-                    if (Directory.Exists(carpetaBasesDatos))
+                    Console.WriteLine($"{nameof(carpeta)}:{carpeta}");
+                    var archivos = Directory.GetFiles(carpeta, "*.fdb");
+                    if (archivos.Any(t => t.Contains("NOI90")))
                     {
-                        Console.WriteLine($"{nameof(carpetaBasesDatos)}:{carpetaBasesDatos}");
-                        var carpetas = Directory.GetDirectories(carpetaBasesDatos);
-                        foreach (var carpeta in carpetas)
+                        var fdb = new FileInfo(archivos.FirstOrDefault(t => t.ToLower().EndsWith(".fdb")));
+                        Console.WriteLine($"{nameof(fdb)}:{fdb}");
+                        File.Move(fdb.FullName, fdb.FullName.Replace("NOI90", "NOI100"));
+                        Console.WriteLine($"{nameof(fdb)}:{fdb.FullName.Replace("NOI90", "NOI100")}");
+                        Console.WriteLine("Finaliza Renombre");
+                    }
+                    if (Directory.Exists(carpetaPlantillas))
+                    {
+                        var carpetaReportes = Path.Combine(carpetaRaiz, $"Datos{contadorCarpetaReportes}");
+                        if (!Directory.Exists(carpetaReportes))
                         {
-                            Console.WriteLine($"{nameof(carpeta)}:{carpeta}");
-                            var archivos = Directory.GetFiles(carpeta, "*.fdb");
-                            if (archivos.Any(t => t.Contains("NOI90")))
-                            {
-                                var fdb = new FileInfo(archivos.FirstOrDefault(t => t.ToLower().EndsWith(".fdb")));
-                                Console.WriteLine($"{nameof(fdb)}:{fdb}");
-                                File.Move(fdb.FullName, fdb.FullName.Replace("NOI90", "NOI100"));
-                                Console.WriteLine($"{nameof(fdb)}:{fdb.FullName.Replace("NOI90", "NOI100")}");
-                                Console.WriteLine("Finaliza Renombre");
-                            }
+                            Directory.CreateDirectory(carpetaReportes);
+                            CopyFilesRecursively(carpetaPlantillas, carpetaReportes);
                         }
                     }
-                    var archivoConexiones = Path.Combine(carpetaRaiz + @"\Conexiones.ini");
-                    if (File.Exists(archivoConexiones))
-                    {
-                        var textoConexiones = File.ReadAllText(archivoConexiones);
-                        textoConexiones = textoConexiones.Replace("NOI9.00", "NOI10.00");
-                        textoConexiones = textoConexiones.Replace("NOI90Empre", "NOI100Empre");
-                        File.WriteAllText(Path.Combine(carpetaRaiz, "Conexiones10.ini"), textoConexiones);
-                        Console.WriteLine($"Finaliza renombrado conexiones");
-                    }
-                    Console.ReadLine();
+                    contadorCarpetaReportes++;
                 }
+            }
+            var archivoConexiones = Path.Combine(carpetaRaiz + @"\Conexiones.ini");
+            if (File.Exists(archivoConexiones))
+            {
+                var textoConexiones = File.ReadAllText(archivoConexiones);
+                textoConexiones = textoConexiones.Replace("NOI9.00", "NOI10.00");
+                textoConexiones = textoConexiones.Replace("NOI90Empre", "NOI100Empre");
+                File.WriteAllText(Path.Combine(carpetaRaiz, "Conexiones10.ini"), textoConexiones);
+                Console.WriteLine($"Finaliza renombrado conexiones");
+            }
+            Console.ReadLine();
+        }
 
+        private static void CopyFilesRecursively(string sourcePath, string targetPath)
+        {
+            //Now Create all of the directories
+            foreach (string dirPath in Directory.GetDirectories(sourcePath, "*", SearchOption.AllDirectories))
+            {
+                Directory.CreateDirectory(dirPath.Replace(sourcePath, targetPath));
+            }
+
+            //Copy all the files & Replaces any files with the same name
+            foreach (string newPath in Directory.GetFiles(sourcePath, "*.*", SearchOption.AllDirectories))
+            {
+                File.Copy(newPath, newPath.Replace(sourcePath, targetPath), true);
             }
         }
     }
--- readme.md
+++ readme.md
@@ -5,11 +5,9 @@ al Dacaspel de la version 9 de NOI y copiar el contenido de la carpeta "Datos" y
 Despues copiamos el archivo Conexiones.ini y empresas.xml de la carpeta de la version 9 y lo pegamos dentro del la carpeta
 de la version 10, respaldando prevaimente dichos archivos en el destino.
 
-Despues mediante linea de comandos ejecutamos el .exe compilado de este proyecto agregando como argumento la ruta de la carpeta
-de la version 10 de COI dentro de Dacaspel.
+Despues pegamos el ejecutable en la carpeta de la version 10 de NOI dentro de Dacaspel.
+Click derecho sobre el ejecutable "Ejecutar como administrador" o se puede ejecutar desde linea de comandos:
 
-Quedando el comando de la siguiente manera: 
-
-.\MIGNOI.exe "C:\Program Files (x86)\Common Files\Aspel\Sistemas Aspel\NOI10.00\"
+.\MIGNOI.exe
 
 Por ultimo renombramos el archvio resultante Conexiones9.ini a Conexiones.ini, respaldando el archivo previamente.
